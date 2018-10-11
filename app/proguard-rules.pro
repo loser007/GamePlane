5fb6bb680f0d9fe97e5fce041e175f92d69ff364
dbSven <dub@iclockwork.com>
2018-10-11 15:09:11 +0800

    下架后重新提交

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,31 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+-optimizationpasses 3
+# 混合时不使用大小写混合，混合后的类名为小写
+-dontusemixedcaseclassnames
+# 指定不去忽略非公共库的类
+-dontskipnonpubliclibraryclasses
+# 这句话能够使我们的项目混淆后产生映射文件 # 包含有类名->混淆后类名的映射关系
+-verbose
+# 指定不去忽略非公共库的类成员
+-dontskipnonpubliclibraryclassmembers
+# 不做预校验，preverify是proguard的四个步骤之一，Android不需要preverify，去掉这一步能够加快混淆速度。
+-dontpreverify
+# 保留Annotation不混淆
+-keepattributes *Annotation*,InnerClasses
+# 避免混淆泛型
+-keepattributes Signature
+# 抛出异常时保留代码行号
+-keepattributes SourceFile,LineNumberTable
+# 指定混淆是采用的算法，后面的参数是一个过滤器 # 这个过滤器是谷歌推荐的算法，一般不做更改
+-optimizations !code/simplification/cast,!field/*,!class/merging/*
+-keep public class * extends android.app.IntentService
+# 保留support下的所有类及其内部类
+-keep class android.support.** {*;}
+# 保留继承的
+-keep public class * extends android.app.Activity
+-keep public class * extends android.support.v4.**
+-keep public class * extends android.support.v7.**
+-keep public class * extends android.support.annotation.**
+-keep class com.android.**{*;}
--- app/src/main/java/com/loser007/gameplane/Data.java
+++ app/src/main/java/com/loser007/gameplane/Data.java
@@ -12,51 +12,51 @@ public class Data {
     public static List<Map<String,String>> Datas = new ArrayList<Map<String,String>>(){
         {
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un1");
+                put("id","com.sair.un1");
                 put("price","打赏$0.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un2");
+                put("id","com.sair.un2");
                 put("price","打赏$2.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un3");
+                put("id","com.sair.un3");
                 put("price","打赏$3.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un4");
+                put("id","com.sair.un4");
                 put("price","打赏$4.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un5");
+                put("id","com.sair.un5");
                 put("price","打赏$9.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un6");
+                put("id","com.sair.un6");
                 put("price","打赏$14.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un7");
+                put("id","com.sair.un7");
                 put("price","打赏$21.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un8");
+                put("id","com.sair.un8");
                 put("price","打赏$28.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un9");
+                put("id","com.sair.un9");
                 put("price","打赏$29.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un10");
+                put("id","com.sair.un10");
                 put("price","打赏$48.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un11");
+                put("id","com.sair.un11");
                 put("price","打赏$49.99");
             }});
             add(new HashMap<String, String>(){{
-                put("id","com.bb.un12");
+                put("id","com.sair.un12");
                 put("price","打赏$99.99");
             }});
 
